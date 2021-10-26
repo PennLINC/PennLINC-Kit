b5b9a2f9b344c482b5b4877b32df669e1ffd1122
Maxwell Bertolero <mbertolero@me.com>
2021-10-26 15:37:09 -0400

    Update data.py

--- pennlinckit/data.py
+++ pennlinckit/data.py
@@ -17,70 +17,23 @@ class self:
 	def __init__(self):
 		pass
 
-def clone(self):
-	"""
-	method of dataset
-	"""
-	orig_dir = os.getcwd()
-	os.makedirs(self.data_path,exist_ok=True)
-	os.chdir(self.data_path)
-	if self.source == 'hcpya': os.system('datalad clone /cbica/home/bertolem/xcp_hcp/fcon/')
-	else: os.system('datalad clone /cbica/projects/RBC/production/{0}/xcp/'.format(self.source.upper()))
-	
-def clone(dataset):
-
-	cmd = 'datalad clone {0}'.format(self.clone_path)
-	os.system(cmd)
-	# $ cd qsiprep_outputs
-	# $ # Unlock one of the results zip files
-	# $ datalad get sub-1_qsiprep-0.14.2.zip
-	# $ datalad unlock sub-1_qsiprep-0.14.2.zip
-	# $ unzip sub-1_qsiprep-0.14.2.zip
-
-	os.chdir('{0}/fcon'.format(self.data_path))
-	os.system('datalad get group_matrices.zip')
-	os.system('datalad unlock group_matrices.zip')
-	os.system('git annex dead here')
 
 class dataset:
 	"""
 	This is the main object to use to load an rbc dataset
-	If the dataset does not exist yet, it will clone & get it, default
-	is to ~/rbc, but you can edit this as 'rbc_path'
-
 	source: str, the name of the dataset
 	cores: int, the number of cores you will use for analysis
-	rbc_path: str, directory, where you want to store, or where you
-	have stored, your rbc data, default is ~/rbc
 	"""
-	def __init__(self, source='pnc',rbc_path=None,source_path=None,cores=1):
-		
-		
-		#just the name of the dataset
-		self.source = source.upper()
-		#there are some functions that use multiple cores
-		self.cores = cores
-		#where does all of your rbc data live?
-		if rbc_path == None: rbc_path='/'.join([expanduser("~"),'rbc'])
-		self.rbc_path = rbc_path
-		#where are we cloning from?
-		if source_path == None: source_path = '/cbica/projects/RBC/production/'
+	def __init__(self, source='hcpya',source_path=None,cores=1):
+
+		self.source = source
+		if self.source == 'hcpya': 
+			source_path = '/cbica/projects/hcpya/'
+			self.subject_measures = pd.read_csv('/cbica/projects/hcpya/unrestricted_mb3152_10_26_2021_13_40_49.csv')
 		self.source_path = source_path
-		#what is the clone path?
-		self.clone_path = '{0}{1}/xcp/output_ria#~data'.format(self.source_path,self.source)
-		#check to see if data exists, if it does not, clone it
-		if os.path.exists('{0}/{1}'.format(self.rbc_path,self.source)) == False:
-			
+		self.cores = cores
 
-		# self.subject_measures #this is going to be the basic demographics csv, age, sex, iq, et cetera
-		# self.session_measures #this is going to be the sessions specific data, motion/qc, params, aquasition
-		# self.data_narratives #this is the history of how we got it into BIDS format pre fmriprep
 
-	def clone():
-		os.makedirs(self.rbc_path)
-		os.chdir(self.rbc_path)
-		cmd = 'datalad clone {0}'.format(self.clone_path)
-		os.system(cmd)
 
 	def update_subjects(self,subjects):
 		self.measures = self.measures[self.measures.subject.isin(subjects)]

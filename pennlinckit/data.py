0b108b1d5c448eec1a3fdf617c9d0bfa3a3f40b6
Max Bertolero <mbertolero@me.com>
2022-05-27 11:02:10 -0400

    comment data.py

--- pennlinckit/data.py
+++ pennlinckit/data.py
@@ -25,6 +25,11 @@ def load(self,loadtype,subjects):
 
 	missing = []
 	for subject in subjects:
+		"""
+		there are a few options here, mostly as we used datasets that were not part of RBC.
+		sooner or later, you can probably remove the hcpd-dcan (after we do hcp-d in RBC).
+		you could always chage the path structure for hcpya to match RBC, but probably not worth the trouble.
+		"""
 		if self.source == 'hcpya':
 			if loadtype == 'cifti': glob_matrices = glob.glob('{0}/DERIVATIVES/XCP/sub-{1}/func/sub-{1}_task-{3}_acq-{4}_space-fsLR_den-91k_desc-residual_den-91k_bold.dtseries.nii'.format(self.source_path,subject,self.task,self.session,self.acq))
 			else: glob_matrices = glob.glob('{0}/DERIVATIVES/XCP/sub-{1}/func/sub-{1}_task-{3}_acq-{4}_space-fsLR_atlas-{5}_den-91k_den-91k_bold.{6}.nii'.format(self.source_path,subject,self.task,self.session,self.acq,self.parcels,name))
@@ -36,19 +41,26 @@ def load(self,loadtype,subjects):
 			glob_qc = glob.glob('/{0}/data/motion/fd/sub-{1}/ses-V1/files/DCANBOLDProc_v4.0.0/analyses_v2/motion/task-{2}_power_2014_FD_only.mat'.format(self.source_path,subject,self.task))
 			glob_scrub = glob.glob('/{0}/data/motion/fd/sub-{1}/ses-V1/files/DCANBOLDProc_v4.0.0/analyses_v2/motion/task-{2}_power_2014_FD_only.mat'.format(self.source_path,subject,self.task))
 		else: #RBC datasets
+			#if we load cifti, that's the entire dense time series. This is very big, but i wanted people to have the option.
 			if loadtype == 'cifti': glob_matrices = glob.glob('{0}/XCP/sub-{1}/{3}/func/sub-{1}_ses-{2}_task-{3}_acq-{4}_space-fsLR_den-91k_desc-residual_den-91k_bold.dtseries.nii'.format(self.source_path,subject,self.task,self.session,self.acq))
+			#in most cases, we will just load the time series, either to pass that directly to the user, or to make the scrubbed matrix.
 			else: glob_matrices = glob.glob('{0}/XCP/sub-{1}/{3}/func/sub-{1}_ses-{2}_task-{3}_acq-{4}_space-fsLR_atlas-{5}_den-91k_den-91k_bold.{6}.nii'.format(self.source_path,subject,self.task,self.session,self.acq,self.parcels,name))
 			glob_qc = glob.glob('{0}/XCP/sub-{1}/{3}/func/sub-{1}_ses-{2}_task-{3}_acq-{4}_space-fsLR_desc-qc_den-91k_bold.csv'.format(self.source_path,subject,self.task,self.session,self.acq))
+			# i call this scrub because it's the FD file, which we use to decide what frames to remove, ie scrub
 			glob_scrub = glob.glob('{0}/XCP/sub-{1}/{3}/func/sub-{1}_ses-{2}_task-{3}_acq-{4}_space-fsLR_desc-framewisedisplacement_den-91k_bold.tsv'.format(self.source_path,subject,self.task,self.session,self.acq))
-		if len(glob_matrices)==0:
+		if len(glob_matrices)==0: # this is mostly to warn people if there are subjects in the demographics that don't have data.
 			print ('missing subject: {0}'.format(subject))
 			missing.append(subject)
 			continue
+			
+		# these are pretty important, might want to build something more robust or a test here. 
+		# I checked that these work, but a change in the name of files could totally wreck that
 		glob_scrub.sort()
 		glob_qc.sort()
 		glob_matrices.sort()
 
-
+		# this is doing scrubbing with the DCAN data. I doubt many people will use this, but this is 
+		# all we had for the time for HCPD, so I wrote it
 		if self.source == 'hcpd-dcan':
 			FD = []
 			for qc in glob_qc:
@@ -65,21 +77,41 @@ def load(self,loadtype,subjects):
 			
 		else:
 			post_scrub_fd = []
+			# going to loop over each time series and scrub file
 			for ts_file,scrub_mask in zip(glob_matrices,glob_scrub):
-				scrub_mask = pd.read_csv(scrub_mask,header=None).values.reshape(1,-1).flatten()
-				ts = nib.load(ts_file).get_fdata()
-				if type(self.fd_scrub) == float:
-					ts = ts[scrub_mask<=self.fd_scrub]
-					post_scrub_fd.append(scrub_mask[scrub_mask<=self.fd_scrub])
-				if 'time_series' in locals(): time_series = np.append(time_series,ts.copy(),axis=0)
-				else: time_series = ts.copy()
+				#load em
+				scrub_mask = pd.read_csv(scrub_mask,header=None).values.reshape(1,-1).flatten() #load the scrub / FD file
+				ts = nib.load(ts_file).get_fdata() #load the time series
+				if type(self.fd_scrub) == float: # if not a float, means no scrubbing.
+					ts = ts[scrub_mask<=self.fd_scrub] #actual scrubbing
+					post_scrub_fd.append(scrub_mask[scrub_mask<=self.fd_scrub]) #save post scrub FC
+
+				# sub cortex is always in a different file, but sometimes we want it all in one big matrix/timeseries
+				# so we do the same thing here as above for the sub-cortical parcels.
+				# this is untested, so I am going to put a break line!
 				if self.sub_cortex:
+					1/0
+					# this needs some editing on second glance,
 					sub_ts = ts_file.replace(parcels,'subcortical')
 					sub_ts = nib.load(sub_ts).get_fdata()
 					if type(self.fd_scrub) == float: 
 						sub_ts = sub_ts[scrub_mask<=self.fd_scrub]
-					time_series = np.append(time_series,sub_ts.copy(),axis=1)
+					ts = np.append(ts,sub_ts.copy(),axis=1)
+				# start to build the actual time series
+				# this little line might be smart or dangerous, not really sure, but we check to see if the time_series exists		
+				if 'time_series' in locals(): time_series = np.append(time_series,ts.copy(),axis=0)
+				# if not, we intantiate it with this first time series
+				else: time_series = ts.copy()
 
+			"""
+			okay, this is pulling the first qc file, getting the colums
+			then it gets the QC files for just the requested tasks/runs/sessions/acq whatever
+			then it gets the mean across them
+			reading this, i was smart to do this. You now have a single QC value for each metric,
+			for the specific BOLD scans you wanted.
+			we also add post-scrubbing FD, which can be useful; when you scrub, you should probably use 
+			this metric to control later analysis instead of the pre-scrub FD. 
+			"""
 			columns = pd.read_csv(glob_qc[0]).columns
 			sub_df = pd.DataFrame(columns=columns)
 			for qc in glob_qc:
@@ -93,7 +125,7 @@ def load(self,loadtype,subjects):
 
 		self.subject_measures.loc[self.subject_measures.subject==subject,self.subject_measures.columns=='n_frames'] = time_series.shape[0]
 		
-		if loadtype == 'matrix': 
+		if loadtype == 'matrix': # if the person wants the matrix, turn it in to matrix
 			subject_matrix = np.corrcoef(time_series.swapaxes(0,1))
 			self.matrix.append(subject_matrix)
 		if loadtype =='ptseries':
@@ -102,10 +134,11 @@ def load(self,loadtype,subjects):
 
 	if self.source != 'hcpd-dcan':self.subject_measures=self.subject_measures.merge(qc_df,on='subject')
 	
-	for missing_sub in missing:
+	for missing_sub in missing: #remove missing subjcets from the dataset object
 		missing_sub = self.subject_measures.loc[self.subject_measures.subject == missing_sub]
 		self.subject_measures = self.subject_measures.drop(missing_sub.index,axis=0)
 
+	#couple sanity checks
 	if loadtype == 'matrix':
 		self.matrix = np.array(self.matrix)
 		assert len(self.matrix) == self.subject_measures.shape[0]
